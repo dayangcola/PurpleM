58e4239e10d8b47e353ebaca5bf3b62041c36a23
dayangcola <yangshuai336@gmail.com>
2025-09-09 01:05:29 +0800

    Initial Commit

---
+++ PurpleM/PurpleMApp.swift
@@ -0,0 +1,32 @@
+//
+//  PurpleMApp.swift
+//  PurpleM
+//
+//  Created by link on 2025/9/9.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PurpleMApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PurpleMTests/PurpleMTests.swift
@@ -0,0 +1,16 @@
+//
+//  PurpleMTests.swift
+//  PurpleMTests
+//
+//  Created by link on 2025/9/9.
+//
+
+import Testing
+
+struct PurpleMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
